9cf14775068e1efb53884fb07f1a951eacab28c0
Mazz <mazz.mosley@digital.cabinet-office.gov.uk>
2012-06-12 15:00:51 +0100

    Fix syntax error.

--- lib/omniauth/strategies/gds.rb
+++ lib/omniauth/strategies/gds.rb
@@ -4,14 +4,18 @@ require 'multi_json'
 class OmniAuth::Strategies::Gds < OmniAuth::Strategies::OAuth2
   uid { user['uid'] }
 
-  info do {
-    name: user['name'],
-    email: user['email']
-  }
+  info do
+    {
+      name: user['name'],
+      email: user['email']
+    }
+  end
 
-  extra do {
-    user: user
-  }
+  extra do
+    {
+      user: user
+    }
+  end
 
   def user
     @user ||= MultiJson.decode(access_token.get('/user.json').body)['user']
